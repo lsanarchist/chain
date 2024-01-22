b1db1c71b7a0603ed9bef1c07f2d6e0d739c439b
lsanarchist <59514149+lsanarchist@users.noreply.github.com>
2024-01-22 20:27:51 +0100

    Delete Facade directory

--- Facade/Client.java
+++
@@ -1,8 +0,0 @@
-// Клієнтський код
-public class Client {
-    public static void main(String[] args) {
-        // Використання Facade для запуску автомобіля
-        CarFacade carFacade = new CarFacade();
-        carFacade.startCar();
-    }
-}
