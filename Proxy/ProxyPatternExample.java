3df0b759063cef850fbc432d3aa4c7f932e9dee3
lsanarchist <59514149+lsanarchist@users.noreply.github.com>
2024-01-22 20:28:28 +0100

    Delete Proxy directory

--- Proxy/ProxyPatternExample.java
+++
@@ -1,8 +0,0 @@
-// Клієнтський код
-public class ProxyPatternExample {
-    public static void main(String[] args) {
-        // Клієнт працює через замісник, не прямо з реальним об'єктом
-        RealObject proxy = new ProxyObject();
-        proxy.performAction();
-    }
-}
